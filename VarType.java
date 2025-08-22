fcb7c34d36913518616ee41cde4b19f870cf32c8
aric08 <aricmar26@crosspointacademy.org>
2025-08-22 16:25:17 +0000

    lesson 2

---
+++ VarType.java
@@ -0,0 +1,12 @@
+public class VarType {
+    public static void main(String[] args) {
+        String s = "bruh";
+        int b = 67;
+        double c = 67.6767;
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(0);
+    }
+}
+
+
